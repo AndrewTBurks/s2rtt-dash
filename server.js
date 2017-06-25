09b7afd9cbba53225b09d02be99a1197c973212e
Andrew Burks <andrewtburks@gmail.com>
2017-06-24 20:54:31 -0500

    Realized I can actually use sflow for docker servers instead of the stupid docker apis :)

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 let request = require('request');
 let express = require('express');
-let Docker = require('node-docker-api').Docker;
-
 let _ = require('lodash');
 
 let app = express();
-let docker = new Docker({
-  socketPath: '/var/run/docker.sock'
-});
 
 let timeData = {};
-let systemState = {};
-let dockerState = {};
+
+let system = {};
+let sage2cloud = {};
 
 app.listen(6969, function() {
   console.log("Dashboard served on 6969")
@@ -22,8 +18,8 @@ app.use(express.static('WWW'));
 app.get('/api/json/', function(req, res) {
   res.type('application/json');
   res.send({
-    system: systemState,
-    docker: dockerState
+    system,
+    sage2cloud
   });
 });
 
@@ -31,7 +27,6 @@ app.get('/api/json/', function(req, res) {
 // start data update cycle
 let dataUpdateInterval = setInterval(function() {
   sFlowDataUpdate();
-  // dockerDataUpdate();
 }, 5000);
 
 // sage2rtt sflow endpoint
@@ -44,37 +39,42 @@ function sFlowDataUpdate() {
     let data = JSON.parse(body);
     // console.log(data);
 
-    let organized = {
+    system = {
       cpu: {},
       gpu: {},
       disk: {}
     };
 
-    _.forEach(_.filter(Object.keys(data), (key) => _.includes(key, "2.1.cpu")), (cpuKey) => {
-      organized.cpu[cpuKey] = data[cpuKey];
+    sage2cloud = {};
+
+    let cputag = "2.1.cpu";
+    _.forEach(_.filter(Object.keys(data), (key) => _.includes(key, cputag)), (cpuKey) => {
+      system.cpu[cpuKey.slice(cputag.length + 1)] = data[cpuKey];
     });
 
+    let gputag = "2.1.nvml";
     _.forEach(_.filter(Object.keys(data), (key) => _.includes(key, "2.1.nvml")), (gpuKey) => {
-      organized.gpu[gpuKey] = data[gpuKey];
+      system.gpu[gpuKey.slice(gputag.length + 1)] = data[gpuKey];
     });
 
+    let disktag = "2.1.disk";
     _.forEach(_.filter(Object.keys(data), (key) => _.includes(key, "2.1.disk")), (diskKey) => {
-      organized.disk[diskKey] = data[diskKey];
+      system.disk[diskKey.slice(disktag.length + 1)] = data[diskKey];
     });
 
-    systemState = organized;
-    // console.log("System State updated", date);
-  });
-}
+    // host ids of processes that are sage2 cloud servers
+    let sage2hostIDs = _.map(
+      _.filter(Object.keys(data), (key) => _.includes(key, ".vir_host_name") && _.includes(data[key], "sage2-")),
+      hostID => hostID.slice(0, -1 * ".vir_host_name".length)
+    );
 
-function dockerDataUpdate() {
-  docker.container.list()
-    .then((containers) => containers.map(container => container.status()))
-    .then((containerStatuses) => containerStatuses.map(containerStatus => containerStatus.stats()))
-    .then((stats) => stats.forEach(stat => {
-      stat.on('data', (stat) => {
-        dockerState = stat;
-      })
-      stat.on('error', (err) => console.log('Error: ', err))
-    }));
+    _.forEach(sage2hostIDs, id => {
+      sage2cloud[id] = {};
+
+      _.forEach(_.filter(Object.keys(data), (key) => _.includes(key, id)), (serverKey) => {
+        sage2cloud[id][serverKey.slice(id.length + 1)] = data[serverKey];
+      });
+    });
+
+  });
 }
